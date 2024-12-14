edc35fd93cf025aae27213b67c6c510a747e1850
alebusta <122423239+alebusta@users.noreply.github.com>
2024-12-14 03:08:56 -0300

    Update Transcriptor.py

--- Transcriptor.py
+++ Transcriptor.py
@@ -180,8 +180,8 @@ def main():
 
         # Determinar tamaño del archivo y ajustar el chunk_size
         file_size_mb = os.path.getsize(temp_audio_path) / (1024 * 1024)
-        chunk_size = 10 * 60000 if file_size_mb > 25 else len(AudioSegment.from_file(temp_audio_path)) 
-        #chunk_size = 10 * 60000 if file_length > 10 else len(AudioSegment.from_file(temp_audio_path))
+        chunk_size = 25 * 60000 if file_size_mb > 25 else len(AudioSegment.from_file(temp_audio_path)) 
+        
 
         # Transcribir el audio
         df_transcription = transcribe_local_audio(temp_audio_path, chunk_size=chunk_size)
