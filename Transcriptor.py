48ba207af2600e11345c547984daab63a96c759b
alebusta <122423239+alebusta@users.noreply.github.com>
2025-01-26 21:58:04 -0300

    Update Transcriptor.py

--- Transcriptor.py
+++ Transcriptor.py
@@ -234,7 +234,7 @@ def main():
             padding: 10px 0;
             z-index: 1000;
             border-bottom: 1px solid #ddd;
-            min-height: 54px;
+            min-height: 80px;
             width:100%;
         }}
         .content-container {{
